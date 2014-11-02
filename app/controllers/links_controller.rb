7f4a36e6f2d3d818e3509cc1caffe957863ada76
jagomez <jgomez.career@gmail.com>
2014-11-02 11:20:35 -0600

    added views and actions in controllers for views

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,24 @@
+class LinksController < ApplicationController
+
+	def index
+		@links = current_user.links
+	end
+	
+	def new
+		@link = Link.new
+	end
+
+	def create
+		@link = current_user.links.new(params[link])
+		if @link.save
+			flash[:success] = "Link Created Successfully"
+			redirect_to links_path
+		else
+			render 'new'
+	end
+
+	def redirect
+		@link = Link.find_by(:slug => params[:slug])
+		redirect_to @link.target_url
+	end
+end
